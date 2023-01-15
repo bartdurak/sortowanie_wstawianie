5ddca4ecf21221c72b9e56fda6015cd051e1b809
bdurak <projektator@gmail.com>
2023-01-15 23:27:57 +0100

    Sortowanie w pythonie

--- insertionsort.py
+++
@@ -1,8 +0,0 @@
-# Sortowanie przez wstawianie
-
-
-from functools import reduce
-from bisect import bisect_left
-
-def insertion_sort(y):
-	return reduce(lambda z, x: [z.insert(bisect_left(z, x), x), z][1], y, [])
